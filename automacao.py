b9c7656bc9e7cb18196277b417d1c217912fb4e7
Felipe Correia Costa <felipe.correia@gkt.grupokabum.com.br>
2025-09-23 13:22:56 -0300

    Second commit

--- automacao.py
+++ automacao.py
@@ -8,12 +8,15 @@ produtos = {
  "placa-mae": "product-card-165133",
  "memoria-ram":"product-card-474939",
  "placa-video":"product-card-723235",
+ "ssd":"product-card-85198",
  "hd":"product-card-112689",
  "cooler":"product-card-248205",
  "fonte":"product-card-369658",
- "gabinete":"product-card-324516",
- "fans":"product-card-96826",
+ "gabinete":"product-card-485726",
+ "fans":"product-card-96826"
  }
+
+produtos_extras = ["monitor", "Extras", "energia", "sistema operacional", "softwares"]
 
 with sync_playwright() as p:
      
@@ -40,6 +43,20 @@ with sync_playwright() as p:
         pagina.get_by_test_id(test_id).click()
         pagina.get_by_test_id("goForwardButton").click()
         time.sleep(2) 
+    
+    #LOGICA PARA PULAR OS PRODUTOS OPCIONAIS
+    for produto in produtos_extras:
+        pagina.get_by_text("Pular").click()
+        print(f"Pulando {produto}...")
+        print("="*50)
+        time.sleep(2)
 
+    print("\n----------------Indo para o resumo----------------")
+    print("=\n"*50)
+    pagina.get_by_test_id("buttonGoToSummary").click()
+    
+    print("\n----------------Indo para o carrinho----------------")
+    pagina.get_by_text("ir para o carrinho").click()
+    input("Aperte ENTER para encerrar:")
     #FECHAMENTO DO NAVEGADOR
 navegador.close()
