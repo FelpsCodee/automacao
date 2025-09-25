f3291a3bb2f9dba539b306c6d3d1b39632828d37
FelpsCodee <felipecorreia781@gmail.com>
2025-09-24 22:27:46 -0300

    third commit

--- automacao.py
+++ automacao.py
@@ -1,6 +1,6 @@
 import credenciais
 import time
-from playwright.sync_api import Playwright, sync_playwright, expect
+from playwright.sync_api import sync_playwright
 
  #LISTAS DOS PRODUTOS PARA O FLUXO
 produtos = {
@@ -20,43 +20,45 @@ produtos_extras = ["monitor", "Extras", "energia", "sistema operacional", "softw
 
 with sync_playwright() as p:
      
- #TIPO DE NAVEGADOR
-    navegador = p.chromium.launch(headless=False) 
+    #TIPO DE NAVEGADOR
+    browser = p.chromium.launch(headless=False) 
+    
     #GERAR UMA PAGINA
-    pagina = navegador.new_page()
+    page = browser.new_page()
     
     #LOGIN
-    pagina.goto("https://www.kabum.com.br/login")
-    pagina.get_by_test_id("login-input").fill(credenciais.usuario)
-    pagina.get_by_test_id("password-input").fill(credenciais.senha)
-    pagina.get_by_test_id("login-submit-button").click()
+    page.goto("https://www.kabum.com.br/login")
+    page.get_by_test_id("login-input").fill(credenciais.usuario)
+    page.get_by_test_id("password-input").fill(credenciais.senha)
+    page.get_by_test_id("login-submit-button").click()
     
     #IR PARA O MONTE O SEU PELO BANNER SUPEIOR DIREITO
-    pagina.wait_for_selector("img[alt='banner_monte_seu_pc']")
-    pagina.click("img[alt='banner_monte_seu_pc']")
-    pagina.get_by_text("Pular").click()
+    page.wait_for_selector("img[alt='banner_monte_seu_pc']")
+    page.click("img[alt='banner_monte_seu_pc']")
+    page.get_by_text("Pular").click()
     
     #LOGICA PARA ESCOLHER PEÇAS NO MONTE O SEU
     for nome, test_id in produtos.items():
         print(f"Adicionando {nome}...")
         print("="*50)
-        pagina.get_by_test_id(test_id).click()
-        pagina.get_by_test_id("goForwardButton").click()
+        page.get_by_test_id(test_id).click()
+        page.get_by_test_id("goForwardButton").click()
         time.sleep(2) 
     
     #LOGICA PARA PULAR OS PRODUTOS OPCIONAIS
     for produto in produtos_extras:
-        pagina.get_by_text("Pular").click()
+        page.get_by_text("Pular").click()
         print(f"Pulando {produto}...")
         print("="*50)
         time.sleep(2)
 
-    print("\n----------------Indo para o resumo----------------")
-    print("=\n"*50)
-    pagina.get_by_test_id("buttonGoToSummary").click()
+    print("\n----------------Indo para o resumo----------------\n")
+
+    page.get_by_test_id("buttonGoToSummary").click()
     
-    print("\n----------------Indo para o carrinho----------------")
-    pagina.get_by_text("ir para o carrinho").click()
+    print("\n---------------Indo para o carrinho---------------\n")
+    page.get_by_text("ir para o carrinho").click()
+    
     input("Aperte ENTER para encerrar:")
-    #FECHAMENTO DO NAVEGADOR
-navegador.close()
+    #FECHAMENTO DO BROWSER
+browser.close()
--- test_automacao_pytest.py
+++ test_automacao_pytest.py
@@ -1,2 +1,4 @@
 import pytest
 
+def test_api_monte_seu_pc():
+   pass
